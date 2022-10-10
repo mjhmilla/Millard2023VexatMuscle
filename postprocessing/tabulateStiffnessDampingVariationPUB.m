86ca66160780da4b044b453e8db90a3cb83b1dfa
mjhmilla <millard.matthew@gmail.com>
2022-10-10 18:53:36 +0200

    correct, un-merged version of tabulateStiffness and extractTables commited

--- postprocessing/tabulateStiffnessDampingVariationPUB.m
+++ postprocessing/tabulateStiffnessDampingVariationPUB.m
@@ -17,7 +17,6 @@ success=0;
                
 freqTable = [15,35,90];
 ampTable = [0.4,0.8,1.6];
-forceTable = 5;
 noDataCode = -42;
 
 kbr1994Data(9) = struct('amplitude',      0,...
@@ -56,33 +55,12 @@ modelVaf(9)   = struct(  'fo', [],...
                          'std', 0,...
                          'min', 0,...
                          'max', 0);
-
-strCaption = '';
-strLabel = '';
-switch flag_useElasticTendon
-    case 0
-        strCaption  = ['Normalized stiffness coefficients (A.), damping coefficients (B.) and VAF (C.) for models with rigid tendons.',...
-                       'As with the elastic tendon models, here the VAF is evalauted between each of the models and the spring-damper',...
-                       'of best fit.'];
-        strLabel    = '\label{tbl:KBR1994Sim_RT}';
-
-    case 1
-        strCaption  = ['Normalized stiffness coefficients (A.), damping coefficients (B.) and VAF (C.) for models',...
-        ' with elastic tendons. Note that the VAF is evaluated between the model and the spring-damper ',...
-        'of best fit to the response of the model, rather than to the response of biological muscle: ',...
-        'Kirsch et al. \cite{Kirsch1994MuscleImpedance} did not publish the time series data from their experiments.'];        
-        strLabel    = '\label{tbl:KBR1994Sim_ET}';
-        
-    otherwise
-        assert(0,'flag_useElasticTendon must be 0 or 1');
-end
                                         
 hillTable   = struct('stiffness',[],'damping',[],'vaf',[],'vafData',[]);
 opus31Table = struct('stiffness',[],'damping',[],'vaf',[],'vafData',[]);
                       
 freq = [ 15,  15,  15,  35,  35,  35,  90,  90,  90];
 amp  = [0.4, 0.8, 1.6, 0.4, 0.8, 1.6, 0.4, 0.8, 1.6];
-force =[  1,   1,   1,   1,   1,   1,   1,   1,   1].*forceTable;
                       
 for i=1:1:length(freq)
   kbr1994(i).amplitude = amp(i);
@@ -111,11 +89,12 @@ for z=1:1:length(freqSeriesFiles)
   currDamping   = modelDamping;
   currVaf       = modelVaf;
 
+ 
 
   for i = 1:1:length(freqSimData.amplitudeMM)
     idx = getIndexIntoVectors(freqSimData.amplitudeMM(1,i),...
-                              freqSimData.bandwidthHz(1,i),amp,freq);                            
-
+                              freqSimData.bandwidthHz(1,i),amp,freq);
+                            
     %If data is being concatenated make sure it is to the correct trial
     if( currData(idx).amplitude ~= 0)
       assert(currData(idx).amplitude == freqSimData.amplitudeMM(1,i));
@@ -152,15 +131,12 @@ for z=1:1:length(freqSeriesFiles)
     
     currData(idx).stiffness_x = k_x;
     currData(idx).stiffness_y = k_y;
-
-    if(length(k_x) > 1)
-        [fo, g] = fit(k_x,k_y,'poly1');    
-        currStiffness(idx).fo = fo;
-        currStiffness(idx).g = g;
-    else
-        currStiffness(idx).fo = k_y;
-        currStiffness(idx).g = 0;
-    end
+    
+    [fo, g] = fit(k_x,k_y,'poly1');
+    
+    currStiffness(idx).fo = fo;
+    currStiffness(idx).g = g;
+    
     % Damping    
     data_x = currData(idx).damping_x;
     data_y = currData(idx).damping_y;
@@ -171,14 +147,10 @@ for z=1:1:length(freqSeriesFiles)
     currData(idx).damping_x = d_x;
     currData(idx).damping_y = d_y;    
     
-    if(length(d_x) > 1)
-        [fo, g] = fit(d_x,d_y,'poly1');
-        currDamping(idx).fo = d_y;
-        currDamping(idx).g = 0;
-    else
-        currDamping(idx).damping_x = d_x;
-        currDamping(idx).damping_y = d_y;    
-    end
+    [fo, g] = fit(d_x,d_y,'poly1');
+    currDamping(idx).fo = fo;
+    currDamping(idx).g = g;
+    
     %VAF
     data_x = currData(idx).vaf_x;
     data_y = currData(idx).vaf_y;
@@ -186,18 +158,12 @@ for z=1:1:length(freqSeriesFiles)
     [vaf_x,map] = sort(data_x);
     vaf_y       = data_y(map);    
     
-    
     currData(idx).vaf_x = vaf_x;
     currData(idx).vaf_y = vaf_y;    
     
-    if(length(vaf_x) > 1)
-        [fo, g] = fit(vaf_x,vaf_y,'poly1');
-        currVaf(idx).fo = fo;
-        currVaf(idx).g = g;
-    else
-        currVaf(idx).fo = vaf_y;
-        currVaf(idx).g = 0;
-    end
+    [fo, g] = fit(vaf_x,vaf_y,'poly1');
+    currVaf(idx).fo = fo;
+    currVaf(idx).g = g;
     currVaf(idx).mean = mean(vaf_y);
     currVaf(idx).std = std(vaf_y);
     currVaf(idx).min = min(vaf_y);
@@ -244,21 +210,10 @@ end
 
 amp9A = [0.4,0.8,1.6];
 freq9A = [15,15,15];
-force9A = [5,5,5];
 assert(length(dataKBR1994Fig9A)==3);
 
 for i=1:1:length(freq9A)
   idx = getIndexIntoVectors(amp9A(1,i),freq9A(1,i),amp,freq);
-%     idx=0;
-%     for z=1:1:length(freqSimData.amplitudeMM)
-%         errA = abs(freqSimData.amplitudeMM(1,z)-amp9A(1,i));
-%         errB = abs(freqSimData.bandwidthHz(1,i)-freq9A(1,i));
-%         errC = abs(freqSimData.nominalForce(1,i)-force9A(1,i));
-%         if(errA < 1e-6 && errB < 1e-6 && errC < 1e-6)
-%             assert(idx==0);
-%             idx=z;
-%         end
-%     end
 
   data_x = [dataKBR1994Fig9A(i).x];
   data_y = [dataKBR1994Fig9A(i).y];
@@ -268,14 +223,9 @@ for i=1:1:length(freq9A)
   kbr1994(idx).stiffness_x = k_x;
   kbr1994(idx).stiffness_y = k_y;
 
-  if(length(k_x) > 1)
-      [fo, g] = fit(k_x,k_y,'poly1');
-      kbr1994Stiffness(idx).fo = fo;
-      kbr1994Stiffness(idx).g  = g;
-  else
-      kbr1994Stiffness(idx).fo = k_y;
-      kbr1994Stiffness(idx).g  = 0;
-  end
+  [fo, g] = fit(k_x,k_y,'poly1');
+  kbr1994Stiffness(idx).fo = fo;
+  kbr1994Stiffness(idx).g  = g;
 end
 
 %%
@@ -283,21 +233,10 @@ end
 %%
 amp9B =  [0.4,0.4,0.4,1.6,1.6,1.6];
 freq9B = [ 15, 35, 90, 15, 35, 90];
-force9B= [  5,  5,  5,  5,  5,  5];
 assert(length(dataKBR1994Fig9B)==6);
 
 for i=1:1:length(freq9B)
   idx = getIndexIntoVectors(amp9B(1,i),freq9B(1,i),amp,freq);
-%     idx=0;
-%     for z=1:1:length(freqSimData.amplitudeMM)
-%         errA = abs(freqSimData.amplitudeMM(1,z)-amp9B(1,i));
-%         errB = abs(freqSimData.bandwidthHz(1,i)-freq9B(1,i));
-%         errC = abs(freqSimData.nominalForce(1,i)-force9B(1,i));
-%         if(errA < 1e-6 && errB < 1e-6 && errC < 1e-6)
-%             assert(idx==0);
-%             idx=z;
-%         end
-%     end
 
   data_x = [dataKBR1994Fig9B(i).x];
   data_y = [dataKBR1994Fig9B(i).y];
@@ -307,14 +246,9 @@ for i=1:1:length(freq9B)
   kbr1994(idx).stiffness_x = k_x;
   kbr1994(idx).stiffness_y = k_y;
 
-  if(length(k_x) > 1)
-      [fo, g] = fit(k_x,k_y,'poly1');
-      kbr1994Stiffness(idx).fo = fo;
-      kbr1994Stiffness(idx).g  = g;
-  else
-      kbr1994Stiffness(idx).fo = k_y;
-      kbr1994Stiffness(idx).g  = 0;      
-  end
+  [fo, g] = fit(k_x,k_y,'poly1');
+  kbr1994Stiffness(idx).fo = fo;
+  kbr1994Stiffness(idx).g  = g;
 end
 
 
@@ -323,7 +257,7 @@ end
 %%
 amp10 =  [0.4,0.4,0.4, 0.8,0.8,0.8, 1.6,1.6,1.6];
 freq10 = [ 15, 35, 90,  15, 35, 90,  15, 35, 90];
-force10= [ 5, 5, 5, 5, 5, 5, 5, 5, 5];
+
 % This figure is a bit of a special case: data from the different
 % perturbations are identically labelled. Kirsch et al. did this to make
 % a point that damping varies with purturbation frequency. However this 
@@ -347,17 +281,7 @@ for i=1:1:length(amp10)
   end
   
   idx = getIndexIntoVectors(amp10(1,i),freq10(1,i),amp,freq);
-%     idx=0;
-%     for z=1:1:length(freqSimData.amplitudeMM)
-%         errA = abs(freqSimData.amplitudeMM(1,z)-amp10(1,i));
-%         errB = abs(freqSimData.bandwidthHz(1,i)-freq10(1,i));
-%         errC = abs(freqSimData.nominalForce(1,i)-force10(1,i));
-%         if(errA < 1e-6 && errB < 1e-6 && errC < 1e-6)
-%             assert(idx==0);
-%             idx=z;
-%         end
-%     end
-    
+
   data_x    = [dataKBR1994Fig10(j).x];
   data_y    = [dataKBR1994Fig10(j).y];
   [d_x,map] = sort(data_x);
@@ -366,15 +290,9 @@ for i=1:1:length(amp10)
   kbr1994(idx).damping_x = d_x;
   kbr1994(idx).damping_y = d_y;
 
-  if(length(d_x)>1)
-      [fo, g] = fit(d_x,d_y,'poly1');
-      kbr1994Damping(idx).fo = fo;
-      kbr1994Damping(idx).g  = g;
-  else
-      kbr1994Damping(idx).fo = d_y;
-      kbr1994Damping(idx).g  = 0;
-  end
-
+  [fo, g] = fit(d_x,d_y,'poly1');
+  kbr1994Damping(idx).fo = fo;
+  kbr1994Damping(idx).g  = g;
   kbr1994(idx).warning = 1;
   kbr1994(idx).warningMessage = 'Grouped data from Fig. 10';
 end
@@ -386,16 +304,7 @@ flag_addInFig12 = 1;
 
 if(flag_addInFig12 == 1)
   idx = getIndexIntoVectors(0.8,35,amp,freq);
-%     idx=0;
-%     for z=1:1:length(freqSimData.amplitudeMM)
-%         errA = abs(freqSimData.amplitudeMM(1,z)-0.8);
-%         errB = abs(freqSimData.bandwidthHz(1,i)-35);
-%         errC = abs(freqSimData.nominalForce(1,i)-5);
-%         if(errA < 1e-6 && errB < 1e-6 && errC < 1e-6)
-%             assert(idx==0);
-%             idx=z;
-%         end
-%     end
+
   assert(length(dataKBR1994Fig12K)==2);
 
   data_x = [dataKBR1994Fig12K(1).x;dataKBR1994Fig12K(2).x];
@@ -406,14 +315,10 @@ if(flag_addInFig12 == 1)
   kbr1994(idx).stiffness_x = k_x;
   kbr1994(idx).stiffness_y = k_y;
 
-  if(length(k_x)>1)
-      [fo, g] = fit(k_x,k_y,'poly1');
-      kbr1994Stiffness(idx).fo = fo;
-      kbr1994Stiffness(idx).g  = g;
-  else
-      kbr1994Stiffness(idx).fo = k_y;
-      kbr1994Stiffness(idx).g  = 0;
-  end
+  [fo, g] = fit(k_x,k_y,'poly1');
+  kbr1994Stiffness(idx).fo = fo;
+  kbr1994Stiffness(idx).g  = g;
+
   data_x = [dataKBR1994Fig12D(1).x;dataKBR1994Fig12D(2).x];
   data_y = [dataKBR1994Fig12D(1).y;dataKBR1994Fig12D(2).y];
   [d_x,map] = sort(data_x);
@@ -422,14 +327,9 @@ if(flag_addInFig12 == 1)
   kbr1994(idx).damping_x = d_x;
   kbr1994(idx).damping_y = d_y;
 
-  if(length(d_x)>1)
-      [fo, g] = fit(d_x,d_y,'poly1');
-      kbr1994Damping(idx).fo = fo;
-      kbr1994Damping(idx).g  = g;
-  else
-      kbr1994Damping(idx).fo = nan;
-      kbr1994Damping(idx).g  = nan;
-  end
+  [fo, g] = fit(d_x,d_y,'poly1');
+  kbr1994Damping(idx).fo = fo;
+  kbr1994Damping(idx).g  = g;
 end
 
 
@@ -475,7 +375,6 @@ end
 
 fid = fopen([outputFolder,'tableStiffnessDampingVaf',tendonTag,'_',tableNameEnding,'.tex'],'w');
 
-
 switch flag_useElasticTendon
     case 0
         strCaption = 'Mean normalized stiffness coefficients (A.), damping coefficients (B.) and VAF (C.) for models with rigid tendons. All additional details are identical to those of Table \label{tbl:KBR1994Sim_ET} except the tendon of the model is rigid.';
@@ -486,6 +385,9 @@ switch flag_useElasticTendon
     otherwise
         assert(0,'flag_useElasticTendon must be 0 or 1');
 end
+
+
+
 
 fprintf(fid,'\\begin{table}[!h]\n');
 fprintf(fid,'\\caption{%s %s}\n',...
@@ -689,4 +591,216 @@ fprintf(fid,'\\end{table}\n');
 
 fclose(fid);
 success=1;
+
+% %%
+% % Transform the table of fitted variables into a tex file
+% %
+% %     KBR 1994    Model      Hill
+% % K/F 15 35 90    15 35 90   15 35 90
+% % 0.4 
+% % 0.8
+% % 1.6
+% %
+% %     KBR 1994    Model      Hill
+% % D/F 15 35 90    15 35 90   15 35 90
+% % 0.4 
+% % 0.8
+% % 1.6
+% %                 Model      Hill
+% % VAF 15 35 90    15 35 90   15 35 90
+% % 0.4 
+% % 0.8 
+% % 1.6
+% %%
+% 
+% tendonTag = '_ElasticTendon';
+% if(flag_useElasticTendon==0)
+%   tendonTag = '_RigidTendon';
+% end
+% 
+% fid = fopen([outputFolder,'tableStiffnessDampingVaf',tendonTag,'_',tableNameEnding,'.tex'],'w');
+% 
+% fprintf(fid,'\\begin{table}\n');
+% fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
+% %            A    15   35   90   15   35   90   15   35   90  
+% 
+% fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
+%             '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
+% fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
+%             '$\frac{K}{F}$', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+% 
+% firstLineExtra = '\hline ';          
+% for i=1:1:length(ampTable)
+%   
+%   kbrK    = cell(1,3);
+%   opus31K = cell(1,3);
+%   hillK   = cell(1,3);
+% 
+%   for j=1:1:3
+%     strVal = '';
+%     if(kbr1994TableStiffness.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.2f',kbr1994TableStiffness.pMean(i,j,1));
+%     end
+%     kbrK(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(opus31Table.stiffness.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.2f',opus31Table.stiffness.pMean(i,j,1));
+%     end
+%     opus31K(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(hillTable.stiffness.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.2f',hillTable.stiffness.pMean(i,j,1));
+%     end
+%     hillK(1,j) = {strVal};    
+%   end
+%   
+%   
+% 
+% 
+%   fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+%                         '& %s & %s & %s ',...
+%                         '& %s & %s & %s \\\\ \n'],...
+%               firstLineExtra, ampTable(i), ...
+%               kbrK{1,1},    kbrK{1,2},    kbrK{1,3},...
+%               opus31K{1,1}, opus31K{1,2}, opus31K{1,3}, ...
+%               hillK{1,1},   hillK{1,2},   hillK{1,3} );
+%   firstLineExtra = '';    
+%   
+% end
+% 
+% fprintf(fid,'\\end{tabular}\n');
+% fprintf(fid,'\\end{table}\n');
+% 
+% fprintf(fid,'\\begin{table}\n');
+% fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
+% %            A    15   35   90   15   35   90   15   35   90  
+% 
+% fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
+%             '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
+% fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
+%             '$\frac{\beta}{F}$', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+% 
+% 
+% firstLineExtra = '\hline ';          
+% for i=1:1:length(ampTable)
+%   
+%   kbrD    = cell(1,3);
+%   opus31D = cell(1,3);
+%   hillD   = cell(1,3);
+% 
+%   for j=1:1:3
+%     strVal = '';
+%     if(kbr1994TableDamping.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.4f',kbr1994TableDamping.pMean(i,j,1));
+%     end
+%     kbrD(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(opus31Table.damping.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.4f',opus31Table.damping.pMean(i,j,1));
+%     end
+%     opus31D(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(hillTable.damping.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.4f',hillTable.damping.pMean(i,j,1));
+%     end
+%     hillD(1,j) = {strVal};    
+%   end
+% 
+% 
+%   fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+%                         '& %s & %s & %s ',...
+%                         '& %s & %s & %s \\\\ \n'],...
+%               firstLineExtra, ampTable(i), ...
+%               kbrD{1,1},    kbrD{1,2},    kbrD{1,3},...
+%               opus31D{1,1}, opus31D{1,2}, opus31D{1,3}, ...
+%               hillD{1,1},   hillD{1,2},   hillD{1,3} );
+%   firstLineExtra = '';    
+%   
+% end
+% 
+% fprintf(fid,'\\end{tabular}\n');
+% fprintf(fid,'\\end{table}\n');
+% 
+% 
+% fprintf(fid,'\\begin{table}\n');
+% fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
+% %            A    15   35   90   15   35   90   15   35   90  
+% 
+% fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
+%             '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
+% fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
+%             'avg. VAF', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+% firstLineExtra = '\hline ';          
+% for i=1:1:length(ampTable)
+% 
+% 
+%   kbrVaf    = cell(1,3);
+%   opus31Vaf = cell(1,3);
+%   hillVaf   = cell(1,3);
+% 
+%   for j=1:1:3
+%     
+%     idx = getIndexIntoVectors(ampTable(1,i),freqTable(1,j),amp,freq);   
+%     
+% %     strVal = '';
+% %     if(kbr1994TableDamping.pMean(i,j,1) == noDataCode)
+% %       strVal = '--';      
+% %     else
+% %       strVal = sprintf('%1.3f',kbr1994TableDamping.pMean(i,j,1));
+% %     end
+%     kbrVaf(1,j) = {''};
+%     
+%     
+%     
+%     strVal = '';
+%     if(opus31Table.vafData(idx).mean == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.1f',opus31Table.vafData(idx).mean*100);
+%     end
+%     opus31Vaf(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(hillTable.vafData(idx).mean == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.1f',hillTable.vafData(idx).mean*100);
+%     end
+%     hillVaf(1,j) = {strVal};    
+%   end
+% 
+% 
+%   fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+%                         '& %s & %s & %s ',...
+%                         '& %s & %s & %s \\\\ \n'],...
+%               firstLineExtra, ampTable(i), ...
+%               kbrVaf{1,1},    kbrVaf{1,2},    kbrVaf{1,3},...
+%               opus31Vaf{1,1}, opus31Vaf{1,2}, opus31Vaf{1,3}, ...
+%               hillVaf{1,1},   hillVaf{1,2},   hillVaf{1,3} );
+%   firstLineExtra = '';     
+% end
+%           
+% 
+% fprintf(fid,'\\end{tabular}\n');
+% fprintf(fid,'\\end{table}\n');
+% 
+% fclose(fid);
+% success=1;
+
 
