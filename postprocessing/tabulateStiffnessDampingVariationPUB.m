0161c1fb0c30648f528a96a8eb1f8df2626b13b8
mjhmilla <millard.matthew@gmail.com>
2022-10-10 18:37:07 +0200

    update to the table generation: improved formatting and added a detailed caption so that I do not get confused in the future

--- postprocessing/tabulateStiffnessDampingVariationPUB.m
+++ postprocessing/tabulateStiffnessDampingVariationPUB.m
@@ -36,12 +36,12 @@ kbr1994Damping(9)   = struct(  'fo', [],...
                       
 modelData(9) = struct(  'amplitude',      0,...
                         'frequency',      0,...
-                        'stiffness_x',    zeros(1,1),...
-                        'stiffness_y',    zeros(1,1),...                        
-                        'damping_x',      zeros(1,1),...
-                        'damping_y',      zeros(1,1),...
-                        'vaf_x',          zeros(1,1),...
-                        'vaf_y',          zeros(1,1));
+                        'stiffness_x',    [],...
+                        'stiffness_y',    [],...                        
+                        'damping_x',      [],...
+                        'damping_y',      [],...
+                        'vaf_x',          [],...
+                        'vaf_y',          []);
                       
 modelStiffness(9) = struct(  'fo', [],...
                               'g', []);    
@@ -88,6 +88,8 @@ for z=1:1:length(freqSeriesFiles)
   currStiffness = modelStiffness;
   currDamping   = modelDamping;
   currVaf       = modelVaf;
+
+ 
 
   for i = 1:1:length(freqSimData.amplitudeMM)
     idx = getIndexIntoVectors(freqSimData.amplitudeMM(1,i),...
@@ -373,14 +375,32 @@ end
 
 fid = fopen([outputFolder,'tableStiffnessDampingVaf',tendonTag,'_',tableNameEnding,'.tex'],'w');
 
-fprintf(fid,'\\begin{table}\n');
+switch flag_useElasticTendon
+    case 0
+        strCaption = 'Mean normalized stiffness coefficients (A.), damping coefficients (B.) and VAF (C.) for models with rigid tendons. All additional details are identical to those of Table \label{tbl:KBR1994Sim_ET} except the tendon of the model is rigid.';
+        strLabel = '\label{tbl:KBR1994Sim_RT}';
+    case 1
+        strCaption = 'Mean normalized stiffness coefficients (A.), damping coefficients (B.) and VAF (C.) for models with elastic tendons. Here the model has been fitted to Figure 12 of Kirsch et al. \cite{Kirsch1994MuscleImpedance}. The impedance experiments at each combination of perturbation amplitude and frequncy have been evaluated at 3 different nominal forces: 2.5N, 5N, and 11.5N. The normalized results presented in the table are the mean values of the 2.5N, 5.0N, and 11.5N simulations. Finally, note that the VAF is evaluated between the model and the spring-damper of best fit to the response of the model, rather than to the response of biological muscle (which was not published by Kirsch et al. \cite{Kirsch1994MuscleImpedance}).';
+        strLabel = '\label{tbl:KBR1994Sim_ET}';
+    otherwise
+        assert(0,'flag_useElasticTendon must be 0 or 1');
+end
+
+
+
+
+fprintf(fid,'\\begin{table}[!h]\n');
+fprintf(fid,'\\caption{%s %s}\n',...
+    strCaption, ...
+    strLabel);
+fprintf(fid,'\\begin{center}\n');
 fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
 %            A    15   35   90   15   35   90   15   35   90  
 
-fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
-            '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
-fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
-            '$\frac{K}{F}$', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+fprintf(fid,['%s & \\multicolumn{3}{c}{Kirsch et al.} & ',...
+            '\\multicolumn{3}{c}{Model} & \\multicolumn{3}{c}{Hill} \\\\ \n'],'');
+fprintf(fid,'%s & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz \\\\ \n',...
+            'A. \hfill Norm. Stiffness ($\frac{K}{F})$', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
 
 firstLineExtra = '\hline ';          
 for i=1:1:length(ampTable)
@@ -418,7 +438,7 @@ for i=1:1:length(ampTable)
   
 
 
-  fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+  fprintf(fid,['%s %1.1f mm & %s & %s & %s ',...
                         '& %s & %s & %s ',...
                         '& %s & %s & %s \\\\ \n'],...
               firstLineExtra, ampTable(i), ...
@@ -429,17 +449,24 @@ for i=1:1:length(ampTable)
   
 end
 
-fprintf(fid,'\\end{tabular}\n');
-fprintf(fid,'\\end{table}\n');
+%fprintf(fid,'\\end{tabular}\n');
+%fprintf(fid,'\\end{center}\n');
+%fprintf(fid,'\\end{table}\n');
 
-fprintf(fid,'\\begin{table}\n');
-fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
+%fprintf(fid,'\\begin{table}[!h]\n');
+%fprintf(fid,'\\caption{%s %s %s}\n',...
+%    'Normalized damping coefficients of', ...
+%    strCaption,...
+%    ['\label{',strLabel,'D}']);
+%fprintf(fid,'\\begin{center}\n');
+%fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
 %            A    15   35   90   15   35   90   15   35   90  
 
-fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
-            '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
-fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
-            '$\frac{\beta}{F}$', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+fprintf(fid,'\\multicolumn{10}{c}{} \\\\ \n');
+%fprintf(fid,['%s & \\multicolumn{3}{c}{Kirsch et al.} & ',...
+%            '\\multicolumn{3}{c}{Model} & \\multicolumn{3}{c}{Hill} \\\\ \n'],'');
+fprintf(fid,'%s & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz \\\\ \n',...
+            'B. \hfill Norm. damping $\frac{\beta}{F}$', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
 
 
 firstLineExtra = '\hline ';          
@@ -476,7 +503,7 @@ for i=1:1:length(ampTable)
   end
 
 
-  fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+  fprintf(fid,['%s %1.1fmm & %s & %s & %s ',...
                         '& %s & %s & %s ',...
                         '& %s & %s & %s \\\\ \n'],...
               firstLineExtra, ampTable(i), ...
@@ -487,18 +514,26 @@ for i=1:1:length(ampTable)
   
 end
 
-fprintf(fid,'\\end{tabular}\n');
-fprintf(fid,'\\end{table}\n');
+%fprintf(fid,'\\end{tabular}\n');
+%fprintf(fid,'\\end{center}\n');
+%fprintf(fid,'\\end{table}\n');
 
 
-fprintf(fid,'\\begin{table}\n');
-fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
+%fprintf(fid,'\\begin{table}[!h]\n');
+%fprintf(fid,'\\caption{%s %s %s}\n','VAF of', ...
+%    strCaption,...
+%    ['\label{',strLabel,'VAF}']);
+%fprintf(fid,'\\begin{center}\n');
+%fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
 %            A    15   35   90   15   35   90   15   35   90  
 
-fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
-            '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
-fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
-            'avg. VAF', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+%fprintf(fid,['%s & \\multicolumn{3}{c}{Kirsch et al.} & ',...
+%            '\\multicolumn{3}{c}{Model} & \\multicolumn{3}{c}{Hill} \\\\ \n'],'');
+
+fprintf(fid,'\\multicolumn{10}{c}{} \\\\ \n');
+
+fprintf(fid,'%s & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz & %dHz \\\\ \n',...
+            'C. VAF (\%) \hfill', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
 firstLineExtra = '\hline ';          
 for i=1:1:length(ampTable)
 
@@ -539,7 +574,7 @@ for i=1:1:length(ampTable)
   end
 
 
-  fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+  fprintf(fid,['%s %1.1fmm & %s & %s & %s ',...
                         '& %s & %s & %s ',...
                         '& %s & %s & %s \\\\ \n'],...
               firstLineExtra, ampTable(i), ...
@@ -551,9 +586,221 @@ end
           
 
 fprintf(fid,'\\end{tabular}\n');
+fprintf(fid,'\\end{center}\n');
 fprintf(fid,'\\end{table}\n');
 
 fclose(fid);
 success=1;
+
+% %%
+% % Transform the table of fitted variables into a tex file
+% %
+% %     KBR 1994    Model      Hill
+% % K/F 15 35 90    15 35 90   15 35 90
+% % 0.4 
+% % 0.8
+% % 1.6
+% %
+% %     KBR 1994    Model      Hill
+% % D/F 15 35 90    15 35 90   15 35 90
+% % 0.4 
+% % 0.8
+% % 1.6
+% %                 Model      Hill
+% % VAF 15 35 90    15 35 90   15 35 90
+% % 0.4 
+% % 0.8 
+% % 1.6
+% %%
+% 
+% tendonTag = '_ElasticTendon';
+% if(flag_useElasticTendon==0)
+%   tendonTag = '_RigidTendon';
+% end
+% 
+% fid = fopen([outputFolder,'tableStiffnessDampingVaf',tendonTag,'_',tableNameEnding,'.tex'],'w');
+% 
+% fprintf(fid,'\\begin{table}\n');
+% fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
+% %            A    15   35   90   15   35   90   15   35   90  
+% 
+% fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
+%             '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
+% fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
+%             '$\frac{K}{F}$', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+% 
+% firstLineExtra = '\hline ';          
+% for i=1:1:length(ampTable)
+%   
+%   kbrK    = cell(1,3);
+%   opus31K = cell(1,3);
+%   hillK   = cell(1,3);
+% 
+%   for j=1:1:3
+%     strVal = '';
+%     if(kbr1994TableStiffness.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.2f',kbr1994TableStiffness.pMean(i,j,1));
+%     end
+%     kbrK(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(opus31Table.stiffness.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.2f',opus31Table.stiffness.pMean(i,j,1));
+%     end
+%     opus31K(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(hillTable.stiffness.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.2f',hillTable.stiffness.pMean(i,j,1));
+%     end
+%     hillK(1,j) = {strVal};    
+%   end
+%   
+%   
+% 
+% 
+%   fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+%                         '& %s & %s & %s ',...
+%                         '& %s & %s & %s \\\\ \n'],...
+%               firstLineExtra, ampTable(i), ...
+%               kbrK{1,1},    kbrK{1,2},    kbrK{1,3},...
+%               opus31K{1,1}, opus31K{1,2}, opus31K{1,3}, ...
+%               hillK{1,1},   hillK{1,2},   hillK{1,3} );
+%   firstLineExtra = '';    
+%   
+% end
+% 
+% fprintf(fid,'\\end{tabular}\n');
+% fprintf(fid,'\\end{table}\n');
+% 
+% fprintf(fid,'\\begin{table}\n');
+% fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
+% %            A    15   35   90   15   35   90   15   35   90  
+% 
+% fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
+%             '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
+% fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
+%             '$\frac{\beta}{F}$', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+% 
+% 
+% firstLineExtra = '\hline ';          
+% for i=1:1:length(ampTable)
+%   
+%   kbrD    = cell(1,3);
+%   opus31D = cell(1,3);
+%   hillD   = cell(1,3);
+% 
+%   for j=1:1:3
+%     strVal = '';
+%     if(kbr1994TableDamping.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.4f',kbr1994TableDamping.pMean(i,j,1));
+%     end
+%     kbrD(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(opus31Table.damping.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.4f',opus31Table.damping.pMean(i,j,1));
+%     end
+%     opus31D(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(hillTable.damping.pMean(i,j,1) == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.4f',hillTable.damping.pMean(i,j,1));
+%     end
+%     hillD(1,j) = {strVal};    
+%   end
+% 
+% 
+%   fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+%                         '& %s & %s & %s ',...
+%                         '& %s & %s & %s \\\\ \n'],...
+%               firstLineExtra, ampTable(i), ...
+%               kbrD{1,1},    kbrD{1,2},    kbrD{1,3},...
+%               opus31D{1,1}, opus31D{1,2}, opus31D{1,3}, ...
+%               hillD{1,1},   hillD{1,2},   hillD{1,3} );
+%   firstLineExtra = '';    
+%   
+% end
+% 
+% fprintf(fid,'\\end{tabular}\n');
+% fprintf(fid,'\\end{table}\n');
+% 
+% 
+% fprintf(fid,'\\begin{table}\n');
+% fprintf(fid,'\\begin{tabular}{r | r r r || r r r || r r r}\n');
+% %            A    15   35   90   15   35   90   15   35   90  
+% 
+% fprintf(fid,['%s & \\multicol{3}{c}{Kirsch et al.} & ',...
+%             '\\multicol{3}{c}{Model} & \\multicol{3}{c}{Hill} \\\\ \n'],'');
+% fprintf(fid,'%s & %d & %d & %d & %d & %d & %d & %d & %d & %d \\\\ \n',...
+%             'avg. VAF', 15, 35, 90, 15, 35, 90, 15, 35, 90 );
+% firstLineExtra = '\hline ';          
+% for i=1:1:length(ampTable)
+% 
+% 
+%   kbrVaf    = cell(1,3);
+%   opus31Vaf = cell(1,3);
+%   hillVaf   = cell(1,3);
+% 
+%   for j=1:1:3
+%     
+%     idx = getIndexIntoVectors(ampTable(1,i),freqTable(1,j),amp,freq);   
+%     
+% %     strVal = '';
+% %     if(kbr1994TableDamping.pMean(i,j,1) == noDataCode)
+% %       strVal = '--';      
+% %     else
+% %       strVal = sprintf('%1.3f',kbr1994TableDamping.pMean(i,j,1));
+% %     end
+%     kbrVaf(1,j) = {''};
+%     
+%     
+%     
+%     strVal = '';
+%     if(opus31Table.vafData(idx).mean == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.1f',opus31Table.vafData(idx).mean*100);
+%     end
+%     opus31Vaf(1,j) = {strVal};
+%     
+%     strVal = '';
+%     if(hillTable.vafData(idx).mean == noDataCode)
+%       strVal = '--';      
+%     else
+%       strVal = sprintf('%1.1f',hillTable.vafData(idx).mean*100);
+%     end
+%     hillVaf(1,j) = {strVal};    
+%   end
+% 
+% 
+%   fprintf(fid,['%s %1.1f & %s & %s & %s ',...
+%                         '& %s & %s & %s ',...
+%                         '& %s & %s & %s \\\\ \n'],...
+%               firstLineExtra, ampTable(i), ...
+%               kbrVaf{1,1},    kbrVaf{1,2},    kbrVaf{1,3},...
+%               opus31Vaf{1,1}, opus31Vaf{1,2}, opus31Vaf{1,3}, ...
+%               hillVaf{1,1},   hillVaf{1,2},   hillVaf{1,3} );
+%   firstLineExtra = '';     
+% end
+%           
+% 
+% fprintf(fid,'\\end{tabular}\n');
+% fprintf(fid,'\\end{table}\n');
+% 
+% fclose(fid);
+% success=1;
 
 
