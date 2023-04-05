a1301a9dbb0c564e2511470d6d86563b7d37cf72
mjhmilla <millard.matthew@gmail.com>
2023-04-05 13:01:01 +0200

    Fixed functions that needed projectFolders as an input but did not have it in its list of arguments.`

--- parameters/updateActiveTitinParameters.m
+++ parameters/updateActiveTitinParameters.m
@@ -111,9 +111,9 @@ for idxParam=1:1:2
                         flag_plot,...
                         flag_useOctave);
 
-    %Will get us within 1% of the best parameter value, as these parameters
+    %Will get us within 2% of the best parameter value, as these parameters
     %are relatively independent
-    for idxBisection=1:1:8
+    for idxBisection=1:1:6
         biParamL = biParam;
         biParamL(idxParam,1) = biParamL(idxParam,1) - biDelta(idxParam,1); 
         errL = errorFcn(biParamL);
