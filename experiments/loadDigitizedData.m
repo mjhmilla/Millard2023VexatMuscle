bcacd7cbae9d456283ab9446e9b7988d713e5139
mjhmilla <millard.matthew@gmail.com>
2023-04-05 09:26:57 +0200

    Updated loadDigitizedData to parse files line by line because in Windows the original use of textscan did not return all of the data but instead just the first entry.

--- experiments/loadDigitizedData.m
+++ experiments/loadDigitizedData.m
@@ -1,24 +1,58 @@
 function data = loadDigitizedData(fileName, xLabel, yLabel, seriesLabels, dataTitle)
 
 data(length(seriesLabels)) = struct('xName','','yName','',...
-                                    'x',zeros(1,1),'y',zeros(1,1),...
+                                    'x',[],'y',[],...
                                     'seriesName','','title',dataTitle);
 
 fid = fopen(fileName);
 
 for i=1:1:length(seriesLabels)
-  labels = textscan(fid,'%s,%s\n');
-  series = textscan(fid,'%f,%f\n');
-  
-  labelStr = labels{1}{1};
-  idComma = strfind(labelStr,',');
-  
   data(i).xName = xLabel;
   data(i).yName = yLabel;
-  data(i).x = series{1};
-  data(i).y = series{2};
   data(i).seriesName = seriesLabels{i};
-  
+
+  line =fgetl(fid);  
+  while( ischar(line) && contains(line,','))
+      
+    idxComma = strfind(line,',');
+    assert(length(idxComma)==1,...
+        'Error: loadDigitized data can only handle files with 2 data columns');
+
+    fieldAStr = line(1,1:1:(idxComma-1));
+    fieldBStr = line((idxComma+1):1:end);
+
+    fieldA = str2double(fieldAStr);
+    fieldB = str2double(fieldBStr);
+
+    if(~isnan(fieldA) && ~isnan(fieldB))
+        data(i).x = [data(i).x;fieldA];
+        data(i).y = [data(i).y;fieldB];        
+    end
+   
+    line =fgetl(fid);
+    
+  end
+
+
 end
+% MM 2023/04/05
+% This works on an installation of Matlab running from Ubuntu, but
+% not in windows: textscan on the windows installation only returns the
+% first data point; on ubuntu it returns all of the data points. 
+%
+% for i=1:1:length(seriesLabels)
+%   labels = textscan(fid,'%s,%s\n');
+%   series = textscan(fid,'%f,%f\n');
+%   
+%   labelStr = labels{1}{1};
+%   idComma = strfind(labelStr,',');
+%   
+%   data(i).xName = xLabel;
+%   data(i).yName = yLabel;
+%   data(i).x = series{1};
+%   data(i).y = series{2};
+%   data(i).seriesName = seriesLabels{i};
+%   
+% end
 
 fclose(fid);
